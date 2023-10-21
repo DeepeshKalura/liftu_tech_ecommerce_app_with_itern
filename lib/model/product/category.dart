16ed1d4dbc270e327a2ae8abd15d9aaaf47199a1
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-10-21 05:46:08 +0530

    created product and categories model from the api

---
+++ lib/model/product/category.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+
+import 'package:collection/collection.dart';
+
+class Category {
+  int? id;
+  String? name;
+  String? image;
+  DateTime? creationAt;
+  DateTime? updatedAt;
+
+  Category({
+    this.id,
+    this.name,
+    this.image,
+    this.creationAt,
+    this.updatedAt,
+  });
+
+  @override
+  String toString() {
+    return 'Category(id: $id, name: $name, image: $image, creationAt: $creationAt, updatedAt: $updatedAt)';
+  }
+
+  factory Category.fromMap(Map<String, dynamic> data) => Category(
+        id: data['id'] as int?,
+        name: data['name'] as String?,
+        image: data['image'] as String?,
+        creationAt: data['creationAt'] == null
+            ? null
+            : DateTime.parse(data['creationAt'] as String),
+        updatedAt: data['updatedAt'] == null
+            ? null
+            : DateTime.parse(data['updatedAt'] as String),
+      );
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'name': name,
+        'image': image,
+        'creationAt': creationAt?.toIso8601String(),
+        'updatedAt': updatedAt?.toIso8601String(),
+      };
+
+  /// `dart:convert`
+  ///
+  /// Parses the string and returns the resulting Json object as [Category].
+  factory Category.fromJson(String data) {
+    return Category.fromMap(json.decode(data) as Map<String, dynamic>);
+  }
+
+  /// `dart:convert`
+  ///
+  /// Converts [Category] to a JSON string.
+  String toJson() => json.encode(toMap());
+
+  Category copyWith({
+    int? id,
+    String? name,
+    String? image,
+    DateTime? creationAt,
+    DateTime? updatedAt,
+  }) {
+    return Category(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      image: image ?? this.image,
+      creationAt: creationAt ?? this.creationAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    if (other is! Category) return false;
+    final mapEquals = const DeepCollectionEquality().equals;
+    return mapEquals(other.toMap(), toMap());
+  }
+
+  @override
+  int get hashCode =>
+      id.hashCode ^
+      name.hashCode ^
+      image.hashCode ^
+      creationAt.hashCode ^
+      updatedAt.hashCode;
+}
---
+++ lib/model/product/product.dart
@@ -0,0 +1,113 @@
+import 'dart:convert';
+
+import 'package:collection/collection.dart';
+
+import 'category.dart';
+
+class Product {
+  int? id;
+  String? title;
+  int? price;
+  String? description;
+  List<String>? images;
+  DateTime? creationAt;
+  DateTime? updatedAt;
+  Category? category;
+
+  Product({
+    this.id,
+    this.title,
+    this.price,
+    this.description,
+    this.images,
+    this.creationAt,
+    this.updatedAt,
+    this.category,
+  });
+
+  @override
+  String toString() {
+    return 'Product(id: $id, title: $title, price: $price, description: $description, images: $images, creationAt: $creationAt, updatedAt: $updatedAt, category: $category)';
+  }
+
+  factory Product.fromMap(Map<String, dynamic> data) => Product(
+        id: data['id'] as int?,
+        title: data['title'] as String?,
+        price: data['price'] as int?,
+        description: data['description'] as String?,
+        images: data['images'] as List<String>?,
+        creationAt: data['creationAt'] == null
+            ? null
+            : DateTime.parse(data['creationAt'] as String),
+        updatedAt: data['updatedAt'] == null
+            ? null
+            : DateTime.parse(data['updatedAt'] as String),
+        category: data['category'] == null
+            ? null
+            : Category.fromMap(data['category'] as Map<String, dynamic>),
+      );
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'title': title,
+        'price': price,
+        'description': description,
+        'images': images,
+        'creationAt': creationAt?.toIso8601String(),
+        'updatedAt': updatedAt?.toIso8601String(),
+        'category': category?.toMap(),
+      };
+
+  /// `dart:convert`
+  ///
+  /// Parses the string and returns the resulting Json object as [Product].
+  factory Product.fromJson(String data) {
+    return Product.fromMap(json.decode(data) as Map<String, dynamic>);
+  }
+
+  /// `dart:convert`
+  ///
+  /// Converts [Product] to a JSON string.
+  String toJson() => json.encode(toMap());
+
+  Product copyWith({
+    int? id,
+    String? title,
+    int? price,
+    String? description,
+    List<String>? images,
+    DateTime? creationAt,
+    DateTime? updatedAt,
+    Category? category,
+  }) {
+    return Product(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      price: price ?? this.price,
+      description: description ?? this.description,
+      images: images ?? this.images,
+      creationAt: creationAt ?? this.creationAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      category: category ?? this.category,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(other, this)) return true;
+    if (other is! Product) return false;
+    final mapEquals = const DeepCollectionEquality().equals;
+    return mapEquals(other.toMap(), toMap());
+  }
+
+  @override
+  int get hashCode =>
+      id.hashCode ^
+      title.hashCode ^
+      price.hashCode ^
+      description.hashCode ^
+      images.hashCode ^
+      creationAt.hashCode ^
+      updatedAt.hashCode ^
+      category.hashCode;
+}
