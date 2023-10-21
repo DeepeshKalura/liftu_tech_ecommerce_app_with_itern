24c206e787e418b682fff8bd6499e968b1a2a939
Deepesh Kalura <deepeshkalurs@gmail.com>
2023-10-21 12:13:27 +0530

    making colors lighter

--- lib/view/global/global_colors.dart
+++ lib/view/global/global_colors.dart
@@ -2,12 +2,12 @@ class GlobalColors {
   static const int red = 0xFFD2606D;
   static const int yellow = 0xFFF4BC66;
   static const int white = 0xFFF8E4DD;
-  static const int orange = 0xFFE07A5F;
+  static const int orange = 0xFFE89C87;
   static const int grey = 0xFF717C84;
-  static const int green = 0xFF81B29A;
+  static const int green = 0xFF9AC1AE;
 
   static int randomColor(int index) {
-    final colors = [red, yellow, orange, grey, green];
+    final colors = [red, yellow, orange, grey, green, orange];
     return colors[index % colors.length];
   }
 }
